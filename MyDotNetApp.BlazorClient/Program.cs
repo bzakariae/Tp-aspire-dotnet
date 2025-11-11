eb5cb22a06c4b595b4537f9bc923e3b1782dc1ed
zakariae bouamama <zakariae2bouamama@gmail.com>
2025-11-11 13:43:53 +0100

    add keycloak tools for authentication

--- MyDotNetApp.BlazorClient/Program.cs
+++ MyDotNetApp.BlazorClient/Program.cs
@@ -1,22 +1,46 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using MyDotNetApp.BlazorClient;
 using MyDotNetApp.BlazorClient.Services;
 using Blazored.LocalStorage;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5222") });
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5222";
+
+
+builder.Services.AddHttpClient("apiservice", client =>
+        client.BaseAddress = new Uri(apiBaseUrl))
+    .AddHttpMessageHandler(sp =>
+        sp.GetRequiredService<AuthorizationMessageHandler>()
+            .ConfigureHandler(
+                authorizedUrls: new[] { apiBaseUrl }
+            ));
+
+
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("apiservice"));
+
 
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<NotificationService>();
 
-var host = builder.Build();
 
-var authService = host.Services.GetRequiredService<AuthService>();
-await authService.InitializeAuth();
+builder.Services.AddOidcAuthentication(options =>
+{
+    var provider = options.ProviderOptions;
+    provider.Authority = "http://localhost:8080/realms/car-rental"; 
+    provider.ClientId = "blazor-client";                            
+    provider.ResponseType = "code";
 
-await host.RunAsync();
+    provider.DefaultScopes.Add("openid");
+    provider.DefaultScopes.Add("profile");
+    provider.DefaultScopes.Add("roles");
+});
+
+await builder.Build().RunAsync();
